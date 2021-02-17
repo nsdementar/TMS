9d52e3f7a07c7b3243a8c5240eada8afe15309ad
ALIAKSANDR PALKHOUSKI <aliaksandr.palkhouski@gmail.com>
2021-02-17 13:49:27 +0300

    Add 4 and 5 options

--- HW6/scripts/hw6.sh
+++ HW6/scripts/hw6.sh
@@ -5,47 +5,82 @@ MAGENTA='\033[0;35m'
 GREEN='\033[0;32m'
 RED='\033[0;31m'
 NORMAL='\033[0m'
-hosts=""
+FQDN="myapp.com"
+ENTER_DIRECTORY=""
+HW6=""
+IP=""
 
 function alphabet_dir {
-    ls -l -X | grep dr | awk '{print "\033[0m"$1,$2,$3,$4,$5,$6,$7,"\033[0;31m"$8,$9"\033[0m"}'| column -t 
+	echo "Enter Directory: "
+	read ENTER_DIRECTORY
+	cd $ENTER_DIRECTORY
+       	ls -l -X | grep dr | awk '{print "\033[0m"$1,$2,$3,$4,$5,$6,$7,"\033[0;31m"$8,$9"\033[0m"}'| column -t 
 }
+
 function alphabet_file {
-    ls -l -X | grep ^- | awk '{print "\033[0m"$1,$2,$3,$4,$5,$6,$7,"\033[0;32m"$8,$9"\033[0m"}'| column -t
+	echo "Enter Directory: "
+	read ENTER_DIRECTORY
+	cd $ENTER_DIRECTORY
+       	ls -l -X | grep ^- | awk '{print "\033[0m"$1,$2,$3,$4,$5,$6,$7,"\033[0;32m"$8,$9"\033[0m"}'| column -t
 } 
+
 function all_stat {
-    echo -e "		    $RED  Number of processes:"
-    ps -aux | wc -l
-    echo "   "
-    echo -e "               $GREEN Load average:"
-    cat /proc/loadavg
-    echo "   "
-    echo -e "               $YELLOW Memory:"
-    free
-    echo "   "
-    echo -e "               $MAGENTA Free disk space:"
-    df -h | grep /dev/mapper/ubuntu |  awk '{ print $1 " " $4"\033[0m" }'
-    echo -e "$NORMAL"
+       	echo -e "	        $RED  Number of processes:"
+       	ps -aux | wc -l
+       	echo "   "
+       	echo -e "               $GREEN Load average:"
+       	cat /proc/loadavg
+       	echo "   "
+       	echo -e "               $YELLOW Memory:"
+	free
+	echo "   "
+	echo -e "               $MAGENTA Free disk space:"
+	df -h | grep /dev/mapper/ubuntu |  awk '{ print $1 " " $4"\033[0m" }'
+	echo -e "$NORMAL"
 }
+
 function hosts_add {
-    echo "Enter IP and FQDN to add /etc/hosts. Example "192.168.3.1 myapp.com" "
-    read hosts 
-    echo "$hosts" >> /home/hosts.log
-        
+	echo "Enter IP for myapp.com  to add /etc/hosts. Example "192.168.3.1" "
+	read IP
+	if grep "$IP" /etc/hosts;
+	then echo "This IP already use"
+	else
+	sudo echo "$IP " " $FQDN" >> /etc/hosts && echo "This IP added."
+	fi
 } 
+
 function hosts_add_overwrite {
-    echo "Yes 5"
-} 
+	echo "Enter IP for myapp.com  to add /etc/hosts. Example "192.168.3.1" "
+        read IP
+        if grep "$IP" /etc/hosts;
+        then echo "This IP already use. Do you want overwrite IP for myapp.com? Y/N"
+		read OVERWRITE
+		case $OVERWRITE in
+			Y)
+				echo "Enter new IP for myapp.com"
+				read NEW_IP
+				sudo echo "$IP " " $FQDN" | sed -i 's/'$IP'/'$NEW_IP'/' /etc/hosts
+				;;
+			N)
+				echo "File /etc/hosts don't update."
+				;;
+		esac
+        else
+        sudo echo "$IP " " $FQDN" >> /etc/hosts && echo "This IP added."
+        fi
+}
+
 function wait_ {
-    echo "Yes 6"
-} 
+	echo "Yes 6"
+}
+
 echo "1. Alphabet directory"
 echo "2. Alphabet files"
 echo "3. All stats"
 echo "4. Add IP and FQDN to /etc/hosts"
 echo "5. Add IP and FQDN to /etc/hosts with overwrite"
 echo "6. Wait directory or file"
-echo -e "select job number [1-6]:)"
+echo -e "select job number [1-6]:"
 read HW6
 
 case $HW6 in
@@ -55,11 +90,11 @@ alphabet_dir
 
 2)
 alphabet_file
-      ;;
+;;
 
 3)
 all_stat
- ;;
+;;
 
 4)
 hosts_add
